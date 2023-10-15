a3aaf6eb35be188cf1477c1b7b0db2ec316e0f30
acurzon <adam@curzon.org>
2023-10-15 22:05:01 +0100

    feat: gh-pages deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  target: 'static',
+  app: {
+    baseURL: '/adam-eats/',
+    buildAssetsDir: 'assets',
+  },
   devtools: { enabled: true },
   css: [`assets/css/main.css`],
   components: [
