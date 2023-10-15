f193c8ba421fa34997b71ce5a8a90faf0c893124
acurzon <adam@curzon.org>
2023-10-15 16:24:02 +0100

    feat: navigation

---
+++ components/navigation.vue
@@ -0,0 +1,10 @@
+<template>
+  <nav>
+    <p>Adam <b>Eats</b></p>
+    <ul>
+      <li><NuxtLink to="/">Home</NuxtLink></li>
+      <li><NuxtLink to="/restaurant">Restaurant</NuxtLink></li>
+      <li><NuxtLink to="/item">Item</NuxtLink></li>
+    </ul>
+  </nav>
+</template>
